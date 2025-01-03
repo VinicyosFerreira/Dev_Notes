608fe578a40bcefc7b49975589bd689d8307b238
Vinicyos Ferreira <vinifr987@gmail.com>
2025-01-03 11:02:00 -0300

    Criando nova lógica para o projeto

--- js/script.js
+++ js/script.js
@@ -1,144 +1,34 @@
 const addNote = document.querySelector('.add-note');
 const containerNotes = document.querySelector('.container-notes');
 const writeNotes = document.getElementById('write-notes');
+const searchNotesBtn = document.querySelector(".bi-search");
+const exportNotesBtn = document.querySelector(".file-export");
 
-// Em formato de array voce usa apenas os indices desejados
-
-// Uma função como molde para criar elementos do DOM
-function createNote(el , parent , content , ...className) {
-    el = document.createElement(el);
-    parent.appendChild(el);
-    el.classList.add(...className);
-    el.textContent = content;
-    return el;
+// Ao carregar página exibe os dados do Local Storage
+function showNotes()  {
+    cleanNotes();
+    reorderNotes();
+    getLocalStorage().forEach(element => {
+        const noteElement = createElement(element.id , element.content , element.fixed);
+        containerNotes.appendChild(noteElement);
+    });
 }
 
-/* 
-    Função que pega notas do LocalStorage, onde é feito um loop para carregar notas
-    salvas, além de invocar as funções das funcionalidades da aplicação
-*/
-function notesLocalStorage() {
-    const classList = ['notes', 'bi' ,'bi-pin' , 'bi-x'];
-    const data = saveLocalStorage('notes');
-
-    for(let i = 0 ; i < data.length ; i++) {
-        createNote('div' , containerNotes , '' ,  classList[0]);
-        createNote('textarea' , containerNotes.children[i] , data[i].content , 'textarea');
-        createNote('i' , containerNotes.children[i], '' ,  classList[1] , classList[2]);
-        createNote('i' , containerNotes.children[i] , '' , classList[1] , classList[3]);
-    }
-
-    deleteNote(containerNotes.children);
-    fixedNote(containerNotes.children);
-    updateNote(containerNotes.children);
-    searchNotes(data);
-    reorderNotes(data);
-}
-
-notesLocalStorage();
-
-/*
- Essa função realiza a manipulação e criação de notas e os elementos contidos dentro, além
- de invocar as devidas funções quando uma nova nota é inserida no Document Object Model e
- persistida no local storage
-*/
-function handleNote() {
-    const classList = ['notes', 'bi' ,'bi-pin' , 'bi-x'];
-
-    createNote('div' , containerNotes , '' ,  classList[0]);
-    const notes = document.querySelectorAll('.notes');
-
-    notes.forEach(element => {
-        if(element.childElementCount < 3) {
-            createNote('textarea' , element , writeNotes.value, 'textarea');
-            createNote('i' , element , '' ,  classList[1] , classList[2]);
-            createNote('i' , element , '' , classList[1] , classList[3]);
-            saveLocalStorage('notes');
-            dataSaveLocalStorage('notes' ,  generateId() , writeNotes.value);
-        }
-    })
-
-    deleteNote(notes);
-    fixedNote(notes);
-    updateNote(notes);
-    searchNotes();
-}
-
-/* Essa função atualiza o valor dos input das notas do localStorage e faz a devida alteração no localStorage
-sincronizado com o DOM
-*/
-
-function updateNote(notes , data) {
-    data = saveLocalStorage('notes')
-    notes = [...notes];
-
-    notes.forEach((element , index) => {
-        element.children[0].addEventListener('input' , (e) => {
-            data[index].content = e.target.value;
-            localStorage.setItem('notes' , JSON.stringify(data));
-        })
-    })
-}
-
-/* Função do LocalStorage que cria o array e chave que salvará os dados */
-function saveLocalStorage(key) {
-    let data = localStorage.getItem(key);
-
-    if(data !== null) {
-        return JSON.parse(data);
-    } else {
-        return data = [];
-    }
-}
-
-// Função que reordena as notas fixas e não fixas permitindo uma prioridade nas notas fixadas
-function reorderNotes(data) {
-    data = saveLocalStorage('notes');
-    data = data.sort((a , b) => {
-        console.log('ta ordenando')
-        return b.fixed - a.fixed
-    })
-    
-    localStorage.setItem('notes' , JSON.stringify(data))
-}
-
-function searchNotes(data) {
-    data = saveLocalStorage('notes');
-    const searchNotes = document.querySelector(".search-notes");
-   
-    searchNotes.children[0].addEventListener('click' , () => {
-        const search = searchNotes.children[1].value;
-        const notesFound = data.filter((element) => {
-            return element.content.toLowerCase().includes(search.toLowerCase());
-        })
-
-        const notes = [...containerNotes.children];
-
-        if(notesFound.length === 0) {
-            return;
-        } 
-
-        notes.forEach((element , index) => {
-            if(!notesFound.includes(data[index])) {
-                element.style.display = 'none';
-            } else {
-                element.style.display = 'flex';
-            }
-        })
-        
-    })
-}
-function dataSaveLocalStorage(key , id , content) {
-    const notesData = saveLocalStorage('notes');
-
+// Criar uma nota 
+function createNote() {
+    const notes = getLocalStorage();
     const objectData = {
-        id : id, 
-        content : content ,
-        fixed : false
+        id: generateId(),
+        content : writeNotes.value,
+        fixed: false
     }
-    
-    notesData.push(objectData);
-    return localStorage.setItem(key , JSON.stringify(notesData));
+
+    const element = createElement(objectData.id , objectData.content , objectData.fixed);
+    containerNotes.appendChild(element);
+
+    notes.push(objectData);
+    saveLocalStorage(notes)
+    writeNotes.value = "";
 }
 
 function generateId () {
@@ -146,76 +36,171 @@ function generateId () {
     return id;
 }
 
-function deleteNote(notes , data) {
-    data = saveLocalStorage('notes');
-    notes = [...notes];
- 
-     notes.forEach((element , index) => {
-        element.dataset.id = data[index].id;
-        const dataId = parseInt(element.dataset.id);
+// Cria os elementos no DOM , além de suas classes e eventos
+function createElement(id , content , fixed) {
+    const notes = document.createElement('div');
+    notes.classList.add('notes');
+    const classList = ['bi-pin' , 'bi-x' , 'bi-file-earmark-plus'];
+    const events = [
+        fixedNotes , 
+        deleteNotes , 
+        copyNotes,
+    ];
 
-        element.children[2].addEventListener('click', () => {
-            element.remove(); 
-            data = data.filter((note) => {
-                return note.id !== dataId;
-            })
-    
-            localStorage.setItem('notes' , JSON.stringify(data));
-            })
-        })
+    const textarea = document.createElement('textarea');
+    textarea.value = content;
+    textarea.placeholder = "Adicione algum texto"
+    notes.appendChild(textarea);
+
+    classList.forEach((el , index) => {
+        const i = document.createElement('i');
+        i.classList.add('bi' , el);
+        notes.appendChild(i);
+        i.addEventListener('click' , () => events[index](id));
+    })
+
+    textarea.addEventListener('input' , (e) => updateNote(e , id));
+
+    if(fixed) {
+       notes.style.backgroundColor = '#333';
+    }
+
+    return notes;
+}
+
+function getLocalStorage(notes) {
+    const data = localStorage.getItem('notes');
+
+    if(data !== null) {
+        return JSON.parse(data);
+    } else {
+        return [];
+    }
+}
+
+// Fazer a limpeza das notas presente no DOM
+function cleanNotes() {
+    containerNotes.replaceChildren([]);
+}
+
+function saveLocalStorage(notes) {
+   localStorage.setItem('notes' , JSON.stringify(notes))
+}
+
+// Reordenar as notas
+function reorderNotes() {
+    const notes = getLocalStorage();
+
+    notes.sort((a , b) => {
+        return b.fixed - a.fixed;
+    })
+    saveLocalStorage(notes);
+}
+
+// Fixar as notas
+function fixedNotes(id) {
+    const notes = getLocalStorage();
+
+    const targetNote = notes.filter((el) => {
+        return id === el.id;
+    })[0];
+
+    targetNote.fixed = !targetNote.fixed;
+    saveLocalStorage(notes);
+    showNotes();
+}
+
+// Atualizar as notas
+function updateNote(e , id) {
+    const notes = getLocalStorage();
+    const targetNote = notes.filter((el) => {
+        return id === el.id;
+    })[0];
+
+    targetNote.content = e.target.value;
+    saveLocalStorage(notes);
+}   
+
+// Deletar a nota
+function deleteNotes(id) {
+    const notes = getLocalStorage();
+
+    const deleteNote = notes.filter((el) => {
+        return id !== el.id;
+    });
+  
+    saveLocalStorage(deleteNote);
+    showNotes();
+}
+
+// Duplicar uma nota
+function copyNotes(id) {
+     const notes = getLocalStorage();
+     
+     const duplicateNote = notes.filter((note) => {
+        return note.id === id;
+     })[0];
+
+    const objectData = {
+        id : generateId(),
+        content: duplicateNote.content,
+        fixed: duplicateNote.fixed
+    }
+
+    const noteElement = createElement(generateId() , objectData.content , objectData.fixed);
+    containerNotes.appendChild(noteElement);
+    notes.push(objectData);
+    saveLocalStorage(notes);
+
+    showNotes();
    
 }
 
-function fixedNote(notes , data) {
-    data = saveLocalStorage('notes');
-    notes = [...notes];
+// Pesquisar uma nota
+function searchNotes() {
+        const notesLocalStorage = getLocalStorage();
+        const notes = document.querySelectorAll('.notes');
+        const search = document.querySelector('.search-notes input').value;
 
-    notes.forEach((element , index) => {
-        const filterNote = notes.filter((note) => {
-            return parseInt(note.dataset.id) === data[index].id;
-        })[0]
+        const notesFound = notesLocalStorage.filter((note) => {
+            return note.content.toLowerCase().includes(search.toLowerCase());
+        })
 
-        if(data[index].fixed) {
-            filterNote.style.backgroundColor = '#333';
-            containerNotes.insertBefore(filterNote , containerNotes.firstChild)
+        if(notesFound.length === 0){
+            return 
         }
 
-        element.children[1].addEventListener('click' , () => {
-         
-            data[index].fixed = !data[index].fixed;
-            console.log(data[index].fixed)
-            if(data[index].fixed) {
-                filterNote.style.backgroundColor = '#333';
-                containerNotes.insertBefore(filterNote , containerNotes.firstChild);
+        notes.forEach((note , index) => {
+            if(!notesFound.includes(notesLocalStorage[index])) {
+                note.style.display = 'none';
             } else {
-                filterNote.style.backgroundColor = '#202124';
-                containerNotes.appendChild(filterNote)
+                note.style.display = 'flex';
             }
+        })
 
-            reorderNotes(notes)
-            localStorage.setItem('notes' , JSON.stringify(data))
-        })    
-    })
 }
 
-function exportNotes(data) {
-    data = saveLocalStorage('notes');
-    const fileExport = document.querySelector('.file-export');
-    fileExport.addEventListener('click' , () => {
-        const csvString = [
-          ["ID" , "Conteúdo" , "Fixado"] ,
-          ...data.map((note) => [note.id , note.content , note.fixed])
-        ]
-        .map((element) => element
-        .join(",")).join("\n") 
-        const element = document.createElement('a');
-        element.href = "data:text/csv;charset=utf-8, " + encodeURI(csvString);
-        element.target = "_blank";
-        element.download = "notes_csv";
-        element.click();
-    })
+// Exportar nota em um documento 
+function exportNotes() {
+    const notes = getLocalStorage();
+
+    const csvString = [
+          ["ID" , "CONTÉUDO" , "FIXADO"],
+        ...notes.map((note) => [note.id , note.content , note.fixed])
+     ].map((element) => element
+    .join(",")).join("\n");
+
+    const element = document.createElement('a');
+    element.href = "data:text/csv;charset=utf-8, " + encodeURI(csvString);
+    element.target = "_blank";
+    element.download = "notes/csv";
+    element.click();
 }
-exportNotes();
 
-addNote.addEventListener('click' , handleNote);
+// Eventos
+addNote.addEventListener('click' , () => createNote());
+exportNotesBtn.addEventListener('click' , () => exportNotes());
+searchNotesBtn.addEventListener('click' , () => searchNotes());
 
+// Invocar show notes
+showNotes();
